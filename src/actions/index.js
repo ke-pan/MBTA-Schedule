25794d99a0bba76260b0e2926edfc4b032bcdc3f
ke-pan <ke_pan@yeah.net>
2016-04-13 08:00:06 +0800

    feat: actions

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+import fetch from 'isomorphic-fetch';
+
+export const REQUEST = 'REQUEST';
+export const FAILURE = 'FAILURE';
+export const SUCCESS = 'SUCCESS';
+
+function receive(data) {
+  return {
+    type: SUCCESS,
+    data
+  }
+}
+
+function request(from, to, day) {
+  return {
+    type: REQUEST,
+    from,
+    to,
+    day,
+  }
+}
+
+function failure() {
+  return {
+    type: FAILURE
+  }
+}
+
+function fetchFare(from, to, day) {
+  return new Promise(function(resolve, reject) {
+    resolve(
+      [{
+        tripId: '01SFO10SUN',
+        fromStopId: 'LAFY',
+        fromDepartureTime: '08:00:00',
+        toStopId: 'ROCK',
+        toArrivalTime: '08:11:00'
+      }]
+    )
+  })
+}
+
+export function loadFare(from, to, day) {
+  return (dispatch) => {
+    dispatch(request(from, to, day));
+    return fetchFare(from, to, day)
+      .then(data => dispatch(receive(data)))
+  }
+}
