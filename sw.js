eea8e5bfee10fdda316bba4f6f01a8cfedb0cc9b
ke-pan <ke_pan@yeah.net>
2016-05-15 12:36:56 +0800

    fix: offline support

--- sw.js
+++ sw.js
@@ -1,33 +1,46 @@
-const staticCacheName = 'static-v1';
-const apiCacheName = 'MBTA-v2';
+const staticCacheName = 'static-v3';
 //
 //
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        'assets/app.js'
+        'assets/app.js',
+        '/'
       ])
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('static-') &&
+            staticCacheName != cacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
 
 self.addEventListener('fetch',function(event) {
   const requestUrl = new URL(event.request.url);
-  if (requestUrl.hostname == "realtime.mbta.com") {
-    event.respondWith(
-      caches.open(apiCacheName).then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          if (response) return response;
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) return response;
 
-          return fetch(event.request).then(function(res) {
-            cache.put(requestUrl, res.clone());
-            return res;
-          })
+        return fetch(event.request).then(function(res) {
+          cache.put(requestUrl, res.clone());
+          return res;
         })
       })
-    )
-  }
+    })
+  )
 
 });
 
